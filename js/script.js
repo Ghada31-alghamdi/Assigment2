5e24f98bef566fac5f986818e1e46ded20f8a721
Ghada31-alghamdi <s202269300@kfupm.edu.sa>
2025-10-23 12:18:51 +0300

    add filter btn to my projuct also imported assigment1 files

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
+const date =new Date();//creating a date object to get today date 
+alert("Welcome to Ghada Protfile The Date is: "+date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear());
+//using alert to show a pop up messige contining welcome and the date using 
+//date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear() methods
+
+const button1 =document.getElementById("all");
+const button2 =document.getElementById("Web_Devalpment");
+const button3 =document.getElementById("Hardwarye_Design");
+const button4 =document.getElementById("System");
+const boxes = document.getElementsByClassName("box");
+
+function filter_projuct(type){
+    for (let i = 0; i < boxes.length; i++) {
+      const element = boxes[i];
+      const boxType = element.getAttribute("data-name");
+      if (type === "all" || boxType === type) {
+        element.classList.remove("hide");
+      } else {
+        element.classList.add("hide");
+      }
+}
+}
+button1.addEventListener("click", function(){filter_projuct("all")})
+button2.addEventListener("click", function(){filter_projuct("Web_Devalpment")})
+button3.addEventListener("click", function(){filter_projuct("Hardwarye_Design")})
+button4.addEventListener("click", function(){filter_projuct("System")})
 
